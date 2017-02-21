fca752b0afa5eadf7e925850d050fdc50999f314
peteski22 <peteski22@gmail.com>
2017-02-21 15:15:00 +0000

    Added more of JKs ideas

--- WebApiDtoMapper/ParameterBinding/MapFromBodyParameterBinding.cs
+++ WebApiDtoMapper/ParameterBinding/MapFromBodyParameterBinding.cs
@@ -1,12 +1,15 @@
 ﻿namespace WebApiDtoMapper.ParameterBinding
 {
+    using Newtonsoft.Json;
     using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     using System.IO;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using System.Web.Http.Controllers;
     using System.Web.Http.Metadata;
-    using Newtonsoft.Json;
     using WebApiDtoMapper;
 
     public class MapFromBodyParameterBinding : HttpParameterBinding
@@ -28,6 +31,14 @@
             var content = await actionContext.Request.Content.ReadAsStreamAsync();
             var reader = new JsonTextReader(new StreamReader(content));
             var deserialized = actionContext.ActionDescriptor.Configuration.Formatters.JsonFormatter.CreateJsonSerializer().Deserialize(reader, _type);
+
+            var validationResults = new List<ValidationResult>();
+
+            if (!Validator.TryValidateObject(deserialized, new ValidationContext(deserialized), validationResults))
+            {
+                validationResults.ForEach(x => actionContext.ModelState.AddModelError(x.MemberNames.First(), x.ErrorMessage));
+            }
+
             var mapper = (IMapper)actionContext.RequestContext.Configuration.DependencyResolver.GetService(typeof(IMapper));
             SetValue(actionContext, mapper.Map(deserialized, _type, Descriptor.ParameterType));
         }
